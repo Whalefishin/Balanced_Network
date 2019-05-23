a871ab4e2a3d03c7e74d49dce08bc5f09f93e467
hhuang2 <hhuang2@swarthmore.edu>
2019-05-22 21:04:09 -0400

    removed graph lab

--- SummerResearch2017/GraphLab/goal.cpp
+++
@@ -1,8 +0,0 @@
-#include "goal.h"
-
-Goal::Goal(std::string location1, std::string location2, std::string message, int points) {
-    this->location1 = location1;
-    this->location2 = location2;
-    this->message = message;
-    this->points = points;
-}
